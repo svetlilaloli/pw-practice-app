bab4af88e75099225bb4588514d44db829ba6e93
Svetlana Vasileva <svetlilaloli@gmail.com>
2024-10-07 07:19:47 +0100

    adding fixtures

--- test-options.ts
+++ test-options.ts
@@ -1,9 +1,25 @@
 import { test as base } from '@playwright/test'
+import { PageManager } from './page-objects/pageManager';
 
 export type TestOptions = {
-    globalsQaUrl: string
+    globalsQaUrl: string,
+    formLayoutsPage: string,
+    pageManager: PageManager
 }
-
+// creating fixture, tests are faster when using fixtures
 export const test = base.extend<TestOptions>({
-    globalsQaUrl: ['', { option: true }]
+    globalsQaUrl: ['', { option: true }],
+    
+    formLayoutsPage: async ({ page }, use) => {
+        await page.goto('/')
+        await page.getByText('Forms').click()
+        await page.getByText('Form Layouts').click()
+        await use('') // the lines before 'use' are executed before the test, the lines after 'use' are executed after the test
+        console.log('Tear down') // 
+    },
+
+    pageManager: async ({ page, formLayoutsPage }, use) => { // passing 'formLayoutsPage' as dependency
+        const pm = new PageManager(page)
+        await use(pm)
+    }
 })
